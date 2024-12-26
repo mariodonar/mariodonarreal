db21cdfe639246b573f747f61adfe142a5581d8e
mariodonar <trichy143official@gmail.com>
2024-12-26 16:54:37 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -153,23 +153,17 @@ async def re_enable_chat(bot, message):
     await message.reply("Chat Successfully re-enabled")
     
 
-@Client.on_message(filters.command('stats') & filters.incoming)
+@Client.on_message(filters.command('stats') & filters.user(ADMINS))
 async def get_ststs(bot, message):
-    if message.from_user.id in ADMINS:
-        rju = await message.reply('Fetching stats..')
-        try:
-            total_users = await db.total_users_count()
-            totl_chats = await db.total_chat_count()
-            files = await Media.count_documents()
-            size = await db.get_db_size()
-            free = 536870912 - size
-            size = get_size(size)
-            free = get_size(free)
-            await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
-        except Exception as e:
-            await rju.edit(f"❌ Failed to fetch stats:\n{e}")
-    else:
-        return await message.reply('⚠️ **Admins Only**')
+    rju = await message.reply('Fetching stats..')
+    total_users = await db.total_users_count()
+    totl_chats = await db.total_chat_count()
+    files = await Media.count_documents()
+    size = await db.get_db_size()
+    free = 536870912 - size
+    size = get_size(size)
+    free = get_size(free)
+    await rju.edit(script.STATUS_TXT.format(files, total_users, totl_chats, size, free))
 
 @Client.on_message(filters.command('invite') & filters.user(ADMINS))
 async def gen_invite(bot, message):
